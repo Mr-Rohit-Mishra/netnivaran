e8a512f5ee542a0b7d974226b2fb2a4adb826182
Mr-Rohit-Mishra <rohitmishraholy@gmail.com>
2023-12-28 11:56:18 +0530

    commit2

--- src/components/About.js
+++ src/components/About.js
@@ -3,32 +3,32 @@ import React, { Component } from 'react'
 export default class About extends Component {
   render() {
     return (
-      <>
-        <p>
+      < >
+        <p style={{color:'black', paddingTop: '6rem'}}>
         Transforming Your Business with Seamless Technology. Experience comprehensive IT services, from strategic planning to implementation and support. Our robust solutions offer unmatched connectivity and real-time monitoring for optimal operations, simplifying your business processes.
         </p>
-        <p>
+        <p style={{color:'black'}}>
         Transforming Your Business with Seamless Technology. Experience comprehensive IT services, from strategic planning to implementation and support. Our robust solutions offer unmatched connectivity and real-time monitoring for optimal operations, simplifying your business processes.
         </p>
-        <p>
+        <p style={{color:'black'}}>
         Transforming Your Business with Seamless Technology. Experience comprehensive IT services, from strategic planning to implementation and support. Our robust solutions offer unmatched connectivity and real-time monitoring for optimal operations, simplifying your business processes.
         </p>
-        <p>
+        <p style={{color:'black'}}>
         Transforming Your Business with Seamless Technology. Experience comprehensive IT services, from strategic planning to implementation and support. Our robust solutions offer unmatched connectivity and real-time monitoring for optimal operations, simplifying your business processes.
         </p>
-        <p>
+        <p style={{color:'black'}}>
         Transforming Your Business with Seamless Technology. Experience comprehensive IT services, from strategic planning to implementation and support. Our robust solutions offer unmatched connectivity and real-time monitoring for optimal operations, simplifying your business processes.
         </p>
-        <p>
+        <p style={{color:'black'}}>
         Transforming Your Business with Seamless Technology. Experience comprehensive IT services, from strategic planning to implementation and support. Our robust solutions offer unmatched connectivity and real-time monitoring for optimal operations, simplifying your business processes.
         </p>
-        <p>
+        <p style={{color:'black'}}>
         Transforming Your Business with Seamless Technology. Experience comprehensive IT services, from strategic planning to implementation and support. Our robust solutions offer unmatched connectivity and real-time monitoring for optimal operations, simplifying your business processes.
         </p>
-        <p>
+        <p style={{color:'black'}}>
         Transforming Your Business with Seamless Technology. Experience comprehensive IT services, from strategic planning to implementation and support. Our robust solutions offer unmatched connectivity and real-time monitoring for optimal operations, simplifying your business processes.
         </p>
-        <p>
+        <p style={{color:'black'}}>
         Transforming Your Business with Seamless Technology. Experience comprehensive IT services, from strategic planning to implementation and support. Our robust solutions offer unmatched connectivity and real-time monitoring for optimal operations, simplifying your business processes.
         </p>
 
